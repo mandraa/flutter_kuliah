aac5d6e1076c3f25674deffb4eedc6f09543713d
mandraa <andra.arema87@gmail.com>
2023-11-01 16:35:03 +0700

    Praktikum Done

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ A new Flutter project.
 
 ## Getting Started
 
+**Nama  : Juniar Andra Permana <br>
+  NIM   : 2141720214 <br>
+  Kls   : TI-3G <br><br>**
+
+
+
 This project is a starting point for a Flutter application.
 
 A few resources to get you started if this is your first Flutter project:
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_kuliah/red_text_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -110,7 +111,21 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            Container(
+              color: Colors.yellowAccent,
+              width: 50,
+              child: const RedTextWidget(
+                text: 'You have pushed the button this many times:',
+              ),
+            ),
+            Container(
+              color: Colors.greenAccent,
+              width: 100,
+              child: const Text(
+                'You have pushed the button this many times:',
+              ),
             ),
           ],
         ),
